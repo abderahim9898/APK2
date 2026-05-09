98f7272995c3f5c406b733b49059307543fc3ff0
abderahim9898 <meskabbi@gmail.com>
2026-05-09 12:14:39 +0100

    Add files via upload

---
+++ app/src/main/java/com/aromarh/acces2/MainActivity.java
@@ -0,0 +1,68 @@
+package com.aromarh.acces2;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.*;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.net.http.SslError;
+
+public class MainActivity extends Activity {
+
+    private WebView webView;
+    private ProgressBar progressBar;
+    private static final String TARGET_URL = "https://aroma-rh.github.io/ACCES2/";
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView     = findViewById(R.id.webview);
+        progressBar = findViewById(R.id.progressBar);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setLoadWithOverviewMode(true);
+        settings.setUseWideViewPort(true);
+        settings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        settings.setBuiltInZoomControls(false);
+        settings.setDisplayZoomControls(false);
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, android.graphics.Bitmap favicon) {
+                progressBar.setVisibility(View.VISIBLE);
+            }
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                progressBar.setVisibility(View.GONE);
+            }
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                handler.proceed();
+            }
+        });
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                progressBar.setProgress(newProgress);
+            }
+        });
+
+        webView.loadUrl(TARGET_URL);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
